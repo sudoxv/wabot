66d6e01a98c73f1b004385a575e93c909ec6b183
sudoxv <salmanajja54@gmail.com>
2025-06-15 10:04:51 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import { default: makeWASocket } from 'baileys';
+
+const startClient = async () => {
+    try {
+        const Options = {
+        }
+    } catch (e) {
+        console.error('Terjadi kesalahan pada saat startClient:', e);
+    }
+}
